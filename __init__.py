84a08d5f1137bcbc7f05b907c736515719772d6f
Kris Gesling <kris.gesling@mycroft.ai>
2020-07-30 19:48:04 +0930

    setup

--- __init__.py
+++ __init__.py
@@ -1,12 +1,18 @@
-from mycroft import MycroftSkill, intent_file_handler
+from mycroft import MycroftSkill
+from mycroft.skills import skill_api_method
 
 
 class SkillApiSource(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
+        self.__name__ = 'skill-api-source-skill'
+    
+    def initialize(self):
+        self.log.info('~~~~~~~~ loaded')
+        self.log.info(skill_api_method)
 
-    @intent_file_handler('source.api.skill.intent')
-    def handle_source_api_skill(self, message):
+    @skill_api_method
+    def speak_from_other_skill(self, message):
         self.speak_dialog('source.api.skill')
 
 
